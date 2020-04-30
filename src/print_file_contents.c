25d1b564d4dd7b138ba34325d6f8becf4f6b18f1
j39m <hikaph@gmail.com>
2020-04-30 12:10:14 -0700

    print_file_contents: add partial UTF-8 support
    This change makes print_file_contents() slightly more aware of UTF-8.
    It does _not_ guard against malformed UTF-8.

--- src/print_file_contents.c
+++ src/print_file_contents.c
@@ -22,10 +22,39 @@ static void *scalloc(size_t size) {
     return result;
 }
 
+/*
+ * Attempts to read |max_chars| (counted in UTF-8 sequences) out of
+ * |buffer| and NUL-terminate the sequence.
+ */
+static void truncate_to_max_chars(char *buffer, size_t max_chars) {
+    int i;
+    size_t cnt;
+    for (i = 0, cnt = 0; cnt < max_chars; cnt++) {
+        unsigned char current = buffer[i] & 0xff;
+        if (current == '\0') {
+            break;
+        } else if (current >> 7 == 0x00) {  // starts with "0b0"
+            i++;
+        } else if (current >> 5 == 0x06) {  // starts with "0b110"
+            i += 2;
+        } else if (current >> 4 == 0x0e) {  // starts with "0b1110"
+            i += 3;
+        } else if (current >> 3 == 0x1e) {  // starts with "0b11110"
+            i += 4;
+        } else {  // possibly malformed UTF-8
+            break;
+        }
+    }
+    buffer[i] = '\0';
+}
+
 void print_file_contents(yajl_gen json_gen, char *buffer, const char *title, const char *path, const char *format, const char *format_bad, const int max_chars) {
     const char *walk = format;
     char *outwalk = buffer;
-    char *buf = scalloc(max_chars * sizeof(char) + 1);
+
+    // UTF-8 sequences are at most 4 bytes each.
+    size_t max_bytes = 4 * max_chars * sizeof(char);
+    char *buf = scalloc(max_bytes + 1);
 
     int n = -1;
     int fd = open(path, O_RDONLY);
@@ -33,7 +62,7 @@ void print_file_contents(yajl_gen json_gen, char *buffer, const char *title, con
     INSTANCE(path);
 
     if (fd > -1) {
-        n = read(fd, buf, max_chars);
+        n = read(fd, buf, max_bytes);
         if (n != -1) {
             buf[n] = '\0';
         }
@@ -43,6 +72,8 @@ void print_file_contents(yajl_gen json_gen, char *buffer, const char *title, con
         walk = format_bad;
         START_COLOR("color_bad");
     }
+
+    truncate_to_max_chars(buf, max_chars);
 
     // remove newline chars
     char *src, *dst;
